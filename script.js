2c3fd396b29ba342c6e58c49d04912d45dfceae9
nguyenne207 <nguyenne.200507@gmail.com>
2021-11-24 16:48:09 +0700

    finish

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+window.addEventListener("load", () => {
+    const btn = document.getElementById('btn')
+    btn.addEventListener('click', () => {
+        alert('Đập đập clmm')
+        const vid = document.getElementById('ldta')
+        vid.play()
+    })
+});
